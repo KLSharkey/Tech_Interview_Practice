4e466aa04aa4c9998bffbf44ed517483af2955a0
Kristen Sharkey <kristen.l.sharkey@gmail.com>
2018-02-01 13:52:09 -0800

    Initial commit

---
+++ app/src/main/java/com/kristensharkey/tech_interview_practice/OtherResources.java
@@ -0,0 +1,13 @@
+package com.kristensharkey.tech_interview_practice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OtherResources extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other_resources);
+    }
+}
